73091592f4b040a93031a522356368fb5f157c54
S3VIC <sewikk@zohomail.eu>
2023-04-06 22:43:01 +0200

    Changed matrix representation to pointer one, first constructor and printing method

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include "matrix.hpp"
 
 int main(void){
-  Matrix<int> matrix;
+  Matrix<int> matrix = Matrix<int>(2,2);
   matrix.test();
+  matrix.print();
   std::cout << "Hello" << std::endl;
   return 0;
 }
